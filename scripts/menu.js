81eb8c4ca19cf7e91ab24d3621c5dbcedd7f5244
Alexandros <ekaratzaferis@gmail.com>
2015-09-21 00:51:54 +0300

    HotFix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1926,7 +1926,7 @@ define([
                     function(){$motifPadlock.children().css('background','#15171b');}
                 );
                 $motifPadlock.removeClass('disabled');
-                $motifPadlock.find('a').attr('data-toggle','button');
+                $motifPadlock.find('a').css('cursor','auto');
             }
         }
         $tangentR.prop('disabled', argument['tangentR']);
