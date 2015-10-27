bbb9226d5bb3ebe30d84868829f36bfd86b1903b
ThanosSar <kwthanos@gmail.com>
2015-10-27 14:36:46 +0200

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -662,7 +662,7 @@ require([
   lattice.onLoad(function(message, lattice) {
     if (_.isObject(lattice)) {
       menu.setLatticeParameters(lattice.defaults);  
-      //menu.setLatticeRestrictions(lattice.restrictions);   
+      menu.setLatticeRestrictions(lattice.restrictions);   
     }
   });
   
