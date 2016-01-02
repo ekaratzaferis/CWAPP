3a83390cc9126e8c83578916e417bac3a1018c39
Alexandros <ekaratzaferis@gmail.com>
2016-01-02 17:57:12 +0200

    Fix

--- scripts/menu_modules/html.js
+++ scripts/menu_modules/html.js
@@ -17,10 +17,41 @@ define([
 {
     // Contructor //
     function userDialog() {
+        // Menu Ribbon //
         
+        // Lattice Tab //
+        
+        // Motif Tab //
+        
+        // Visual Tab //
+        
+        // PnD Tab //
+        
+        // Public Library Tab //
+        
+        // IAC //
+        this.iac = {};
+        this.iac.toggleVisibility = jQuery('#iacToggle');
+        this.iac.atomSymbol = jQuery('#iacSymbol');
+        this.iac.atomRadius = jQuery('#radiusLabelValue');
+        this.iac.atomRadiusLabel = jQuery('#radiusLabel');
+        this.iac.doll = jQuery('#iacDoll');
+        this.iac.colorPicker = jQuery('#iacColor');
+        
+        
+        var $opacity = jQuery('#iacOpacity');
+        var $opacitySlider = jQuery('#iacOpacitySlider');
+        var $notes = jQuery('#iacNotes');
+        var $sound = jQuery('#iacSound');
+        var $box = jQuery('#iacBox');
+        var $closeButton = jQuery('#iacClose');
+        var $boxTitle = jQuery('#iacLabel h2');
+        // Modals //
+        
+        // Notes //
+        
+        // Other //
     };
-    
-    var alex = 4;
     
     return userDialog;
 });
